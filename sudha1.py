7f5c4be898bcc259b4ba3723ee619843353e682b
sudharshanreddy199 <162984671+sudharshanreddy199@users.noreply.github.com>
2024-03-11 16:17:15 +0530

    Add files via upload

---
+++ sudha1.py
@@ -0,0 +1,92 @@
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+"""
+1.} commenting line is used for making documentation in python file
+2.} when the line is commented,that line will not be considered as a part of the code
+3.}when the program is excecuted, commented line will not be in the part of the excecution
+-------->single line commenting can be done using# at the beginning of the line
+ eg: #print("hello world")
+----> multiple line commenting  can be done using""" """ the lines b/w quoytes will be commented
+"""
+eg:"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+#print("hello world")
+import this
+import antigravity
+
+"""
+bargaind characters
+
+contrals and data structures- abc language
+string nd its formats- from c
+regular expressions-pearl
+exception handling-m0dule3
+magic methods from -fortron
+"""
+
+"""
+ Flavours of Python
+
+- Cpython - Written in C. Most common Implementation of python,
+
+- Jython - Java implementation of Python
+
+- IronPython - implementation of the Python programming language which is tightly integrated with NET.
+
+- Brython-Browser Python, runs in browsers
+
+- RubyPython-Bridge between Python and Ruby Interpreters
+
+- MicroPython - Runs in Micro Controller's
+"""
+
+"""
+# ----> variables
+ --->variable is a name which is used to identify the value of the variable
+---> ?which is stored in in specific memeory location
+# rules to define variables
+#1.--> variable have to start with only alphabets,underscores
+#2.--> the char following the 1st char can be alphabets,numbers,underscores
+#3.--> no spaces are allowed
+#4.--> no special charecters  like @#%^&%&$%
+#5.--> case sensitive
+#6.-->keywords are not allowed as variables
+"""
+
+
+a=8
+b=9
+print(a,b)
+
+a=7,b=9#error
+prin(a,b)
+a,b+7,9
+print(a)
+print(b)
+a=7
+b=7
+a=b=7
+print(a,b)
+a=8,7,9.89,"hello"
+print(a)
+print(type(a))
+a,b=78 #error
+print(a,b)
